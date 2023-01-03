f09217c87c47817a5e4f38fc570d4a0d7d879623
crlsh <crlshgimenez@gmail.com>
2023-01-03 19:09:00 -0300

    initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_8_UyQNPIX6-nm02Al1hFuuz3O-OSDMo",
+  authDomain: "lplog-31164.firebaseapp.com",
+  projectId: "lplog-31164",
+  storageBucket: "lplog-31164.appspot.com",
+  messagingSenderId: "665246214539",
+  appId: "1:665246214539:web:230a8899ff2995db53e78e"
+};
