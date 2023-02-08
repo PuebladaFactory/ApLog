e2a8317c4fa097c627f8da0f5ceb554f3caefe82
Nicolas Gamarra <nicogamarra2010@gmail.com>
2023-02-08 12:48:18 -0300

    choferes-legajos

--- src/app/admin/choferes/choferes-routing.module.ts
+++ src/app/admin/choferes/choferes-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChoferesAltaComponent } from './choferes-alta/choferes-alta.component';
 import { ChoferesBajaComponent } from './choferes-baja/choferes-baja.component';
 import { ChoferesControlComponent } from './choferes-control/choferes-control.component';
+import { ChoferesLegajoComponent } from './choferes-legajo/choferes-legajo.component';
 import { ChoferesListadoComponent } from './choferes-listado/choferes-listado.component';
 import { ChoferesTarifaComponent } from './choferes-tarifa/choferes-tarifa.component';
 
@@ -12,7 +13,8 @@ const routes: Routes = [
     {path: 'alta', component:ChoferesAltaComponent},
     {path: 'baja', component:ChoferesBajaComponent},
     {path: 'listado', component:ChoferesListadoComponent},
-    {path: 'tarifa', component:ChoferesTarifaComponent}
+    {path: 'tarifa', component:ChoferesTarifaComponent},
+    {path: 'legajo', component:ChoferesLegajoComponent}
 ]  },
   
 ];
