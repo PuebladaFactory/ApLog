49b6489f2d51f722a3365aba161cc1a8b4486adf
Nicolas Gamarra <nicogamarra2010@gmail.com>
2023-09-18 17:41:10 -0300

    facturacion formato tabla

--- src/app/admin/facturacion/facturacion.module.ts
+++ src/app/admin/facturacion/facturacion.module.ts
@@ -5,13 +5,15 @@ import { FacturacionRoutingModule } from './facturacion-routing.module';
 import { FacturacionClienteComponent } from './facturacion-cliente/facturacion-cliente.component';
 import { FacturacionChoferComponent } from './facturacion-chofer/facturacion-chofer.component';
 import { FacturacionGeneralComponent } from './facturacion-general/facturacion-general.component';
+import { FacturacionConsultaComponent } from './facturacion-consulta/facturacion-consulta.component';
 
 
 @NgModule({
   declarations: [
     FacturacionClienteComponent,
     FacturacionChoferComponent,
-    FacturacionGeneralComponent
+    FacturacionGeneralComponent,
+    FacturacionConsultaComponent
   ],
   imports: [
     CommonModule,
