9bde8b91f0a23853328db0206e57f6ccdefaf97d
Nicolas Gamarra <nicogamarra2010@gmail.com>
2024-02-16 17:53:46 -0300

    modulo facturacion

--- src/app/admin/facturacion/facturacion-cliente/facturacion-cliente.component.ts
+++ src/app/admin/facturacion/facturacion-cliente/facturacion-cliente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/interfaces/cliente';
 import { FacturaOpCliente } from 'src/app/interfaces/factura-op-cliente';
+import { ConsultasService } from 'src/app/servicios/consultas/consultas.service';
 import { StorageService } from 'src/app/servicios/storage/storage.service';
 
 @Component({
@@ -11,7 +12,8 @@ import { StorageService } from 'src/app/servicios/storage/storage.service';
 export class FacturacionClienteComponent implements OnInit  {
 
   btnConsulta:boolean = false;
-  searchText!:string
+  searchText!:string;
+  searchText2!:string;
   fechasConsulta: any = {
     fechaDesde: 0,
     fechaHasta: 0,
@@ -21,14 +23,23 @@ export class FacturacionClienteComponent implements OnInit  {
   primerDia: any = new Date(this.date.getFullYear(), this.date.getMonth() , 1).toISOString().split('T')[0];
   ultimoDia:any = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).toISOString().split('T')[0];  
   datosTabla: any[] = [];
+  mostrarTabla: boolean[] = [];
+  tablaDetalle: any[] = [];
   
-  constructor(private storageService: StorageService){}
+  constructor(private storageService: StorageService, private consultasService:ConsultasService){
+    // Inicializar el array para que todos los botones muestren la tabla cerrada al principio
+    this.mostrarTabla = new Array(this.datosTabla.length).fill(false);
+  }
 
   ngOnInit(): void {
+  
+    this.consultasService.consultaOperaciones("facturaOpCliente", "fecha", this.primerDia, this.ultimoDia, "consultasFacOpCliente" );
     this.storageService.consultasFacOpCliente$.subscribe(data => {
       this.$facturasOpCliente = data;
+      this.procesarDatosParaTabla()
     });
-    this.consultaMes(); 
+    
+    //this.consultaMes(); 
   }
 
   consultaMes(){
@@ -79,10 +90,28 @@ export class FacturacionClienteComponent implements OnInit  {
     });
 
     this.datosTabla = Array.from(clientesMap.values());
-    console.log("Datos para la tabla: ", this.datosTabla);
+    console.log("Datos para la tabla: ", this.datosTabla); 
     
   }
+ 
+  liquidarFac(factura: FacturaOpCliente){
+    console.log("esta es la FACTURA: ", factura);
+    factura.liquidacion = true;
+    this.procesarDatosParaTabla();
+     
+  }
 
+  mostrarMasDatos(index: number, cliente:any) {
+    // Cambiar el estado del botón en la posición indicada
+    this.mostrarTabla[index] = !this.mostrarTabla[index];
+    console.log("CLIENTE: ", cliente);
+    this.tablaDetalle = this.$facturasOpCliente.filter(function (factura: FacturaOpCliente) { 
+      return factura.idCliente === cliente.idCliente
+    });
+    console.log("TABLA DETALLE: ", this.tablaDetalle);
+    
+    
+  }
 
   
 }
