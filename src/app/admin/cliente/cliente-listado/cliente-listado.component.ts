29a8ab040d9c25dfcc5072a07cf7c6de1bdcfa57
Nicolas Gamarra <nicogamarra2010@gmail.com>
2024-06-21 23:15:29 -0300

    moduloClientesTablas

--- src/app/admin/cliente/cliente-listado/cliente-listado.component.ts
+++ src/app/admin/cliente/cliente-listado/cliente-listado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from 'src/app/appLogin/login/login.component';
@@ -7,6 +7,7 @@ import { StorageService } from 'src/app/servicios/storage/storage.service';
 import { OpAltaComponent } from '../../operaciones/op-alta/op-alta.component';
 import { ClienteAltaComponent } from '../cliente-alta/cliente-alta.component';
 import Swal from 'sweetalert2';
+import { ColumnMode, SelectionType, SortType } from '@swimlane/ngx-datatable';
 
 
 @Component({
@@ -17,7 +18,7 @@ import Swal from 'sweetalert2';
 export class ClienteListadoComponent implements OnInit {
   
   searchText!:string;
-  clientes$!: any;
+  clientes$!: Cliente[];
   edicion:boolean = false;
   clienteEditar!: Cliente;
   form:any;
@@ -28,7 +29,38 @@ export class ClienteListadoComponent implements OnInit {
   indice!:number;
   $clientes:any;
   soloVista: boolean = false;
+////////////////////////////////////////////////////////////////////////////////////////
+@ViewChild('tablaClientes') table: any;  
+rows: any[] = [];
+filteredRows: any[] = [];
+paginatedRows: any[] = [];
+allColumns = [
+//    { prop: '', name: '', selected: true, flexGrow:1  },
+  { prop: 'idCliente', name: 'Id Cliente', selected: false, flexGrow:2  },  
+  { prop: 'razonSocial', name: 'Razon Social', selected: true, flexGrow:2  },          
+  { prop: 'cuit', name: 'CUIT', selected: true, flexGrow:2  },
+  { prop: 'direccion', name: 'Direccion', selected: true, flexGrow:2 },
+  
+  { prop: 'contacto', name: 'Contacto', selected: true, flexGrow:2  },    
+  { prop: 'puesto', name: 'Puesto', selected: true, flexGrow:2  },    
+  { prop: 'telefono', name: 'N° Contacto', selected: true, flexGrow:2  },    
+  { prop: 'correo', name: 'Correo', selected: true, flexGrow:2  },  
+  
+];
+visibleColumns = this.allColumns.filter(column => column.selected);
+selected = [];
+count = 0;
+limit = 20;
+offset = 0;
+sortType = SortType.multi; // Aquí usamos la enumeración SortType
+selectionType = SelectionType.checkbox; // Aquí usamos la enumeración SelectionType
+ColumnMode = ColumnMode;  
+encapsulation!: ViewEncapsulation.None;
+ajustes: boolean = false;
+firstFilter = '';
+secondFilter = '';
 
+//////////////////////////////////////////////////////////////////////////////////////////
 
   constructor(private fb: FormBuilder, private storageService: StorageService,  private modalService: NgbModal){
     this.form = this.fb.group({      
@@ -52,15 +84,17 @@ export class ClienteListadoComponent implements OnInit {
   }
   
   ngOnInit(): void { 
-    //this.clientes$ = this.storageService.clientes$; 
+    
     this.storageService.clientes$.subscribe(data => {
       this.$clientes = data;
+      this.armarTabla();
     })
   }
   
-  abrirEdicion(cliente:Cliente):void {
+  abrirEdicion(row:any):void {
+    this.seleccionarCliente(row);
     this.soloVista = false;
-    this.clienteEditar = cliente;    
+    //this.clienteEditar = cliente;    
     ////console.log()("este es el cliente a editar: ", this.clienteEditar);
     this.armarForm();    
   }
@@ -75,9 +109,16 @@ export class ClienteListadoComponent implements OnInit {
     })
   }
 
-  abrirVista(cliente:Cliente):void {
-    this.soloVista = true;
-    this.clienteEditar = cliente;    
+  seleccionarCliente(row:any){ 
+    let seleccion = this.$clientes.filter((cliente:Cliente)=>{
+      return cliente.idCliente === row.idCliente
+    })
+    this.clienteEditar = seleccion[0];    
+  }
+
+  abrirVista(row:any):void {
+    this.seleccionarCliente(row)
+    this.soloVista = true;    
     ////console.log()("este es el cliente a editar: ", this.clienteEditar);
     this.armarForm();    
   }
@@ -169,7 +210,8 @@ export class ClienteListadoComponent implements OnInit {
   /* toogleMostrar(){
     this.mostrar = !this.mostrar;
   } */
-  eliminarCliente(cliente: Cliente){
+  eliminarCliente(row:any){
+    this.seleccionarCliente(row)
     Swal.fire({
       title: "¿Eliminar el Cliente?",
       text: "No se podrá revertir esta acción",
@@ -181,7 +223,7 @@ export class ClienteListadoComponent implements OnInit {
       cancelButtonText: "Cancelar"
     }).then((result) => {
       if (result.isConfirmed) {
-        this.storageService.deleteItem(this.componente, cliente);
+        this.storageService.deleteItem(this.componente, this.clienteEditar);
         Swal.fire({
           title: "Confirmado",
           text: "El Cliente ha sido borrado",
@@ -226,6 +268,87 @@ export class ClienteListadoComponent implements OnInit {
         (reason) => {}
       );
     }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////
+  armarTabla() {
+    //console.log("consultasOp: ", this.$consultasOp );
+    let indice = 0
+    this.rows = this.$clientes.map((cliente: Cliente) => ({
+        indice: indice ++,
+        idCliente: cliente.idCliente,
+        razonSocial: cliente.razonSocial,
+        direccion: cliente.direccion,
+        cuit: cliente.cuit,
+        contacto: cliente.contactos.length > 0 ? cliente.contactos[0].apellido : "Sin Datos",
+        puesto: cliente.contactos.length > 0 ? cliente.contactos[0].puesto : "Sin Datos" ,
+        telefono: cliente.contactos.length > 0 ? cliente.contactos[0].telefono : "Sin Datos"  ,
+        correo: cliente.contactos.length > 0 ? cliente.contactos[0].email : "Sin Datos",
+        
+      }));
+    
+    //console.log("Rows: ", this.rows); // Verifica que `this.rows` tenga datos correctos
+    this.applyFilters(); // Aplica filtros y actualiza filteredRows
+  }
+  
+  setPage(pageInfo: any) {
+    this.offset = pageInfo.offset;
+    this.updatePaginatedRows();
+  }
+  
+  updatePaginatedRows() {
+    const start = this.offset * this.limit;
+    const end = start + this.limit;
+    this.paginatedRows = this.filteredRows.slice(start, end);
+  }
+  
+  onSort(event:any) {
+    // Implementa la lógica de ordenamiento
+  }
+  
+  onActivate(event: any) {
+    // Implementa la lógica de activación de filas
+  }
+  
+  onSelect(event: any) {
+    // Implementa la lógica de selección de filas
+  }
+  
+  updateFilter(event: any, filterType: string) {
+    const val = event.target.value.toLowerCase();
+    if (filterType === 'first') {
+      this.firstFilter = val;
+    } else if (filterType === 'second') {
+      this.secondFilter = val;
+    }
+    this.applyFilters();
+  }
+  
+  applyFilters() {
+    this.filteredRows = this.rows.filter(row => {
+      const firstCondition = Object.values(row).some(value => 
+        String(value).toLowerCase().includes(this.firstFilter)
+      );
+      const secondCondition = Object.values(row).some(value => 
+        String(value).toLowerCase().includes(this.secondFilter)
+      );
+  
+      return firstCondition && secondCondition;
+    });
+  
+    this.count = this.filteredRows.length; // Actualiza el conteo de filas
+    this.setPage({ offset: this.offset }); // Actualiza los datos para la página actual
+  }
+  
+  toggleColumn(column: any) {
+    if (column.prop !== '') { // Siempre muestra la columna del botón
+      column.selected = !column.selected;
+    }
+    this.visibleColumns = this.allColumns.filter(col => col.selected);
+  }
+  
+  toogleAjustes(){
+    this.ajustes = !this.ajustes;
   }
 
 }
--- src/app/admin/cliente/cliente.module.ts
+++ src/app/admin/cliente/cliente.module.ts
@@ -5,7 +5,7 @@ import { ClienteRoutingModule } from './cliente-routing.module';
 import { ClienteControlComponent } from './cliente-control/cliente-control.component';
 import { ClienteListadoComponent } from './cliente-listado/cliente-listado.component';
 import { ClienteAltaComponent } from './cliente-alta/cliente-alta.component';
-
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
@@ -33,7 +33,8 @@ import { ModalAltaTarifaComponent } from './modal-alta-tarifa/modal-alta-tarifa.
     FormsModule,
     ReactiveFormsModule,
     SharedModule,
-    Ng2SearchPipeModule
+    Ng2SearchPipeModule,
+    NgxDatatableModule
   ]
 })
 export class ClienteModule { }
