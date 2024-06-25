e0cffebd25f029fa56fb993b4a724470514abef9
Nicolas Gamarra <nicogamarra2010@gmail.com>
2024-06-25 12:51:53 -0300

    moduloHistorial: cambios en las consultas a los observables

--- src/app/admin/facturacion/historial/modal-detalle/modal-detalle.component.ts
+++ src/app/admin/facturacion/historial/modal-detalle/modal-detalle.component.ts
@@ -7,6 +7,7 @@ import { TarifaChofer } from 'src/app/interfaces/tarifa-chofer';
 import { TarifaCliente } from 'src/app/interfaces/tarifa-cliente';
 import { TarifaProveedor } from 'src/app/interfaces/tarifa-proveedor';
 import { BuscarTarifaService } from 'src/app/servicios/buscarTarifa/buscar-tarifa.service';
+import { StorageService } from 'src/app/servicios/storage/storage.service';
 
 @Component({
   selector: 'app-modal-detalle',
@@ -24,22 +25,32 @@ export class ModalDetalleComponent implements OnInit {
   totalFacturaCliente!:number;
   totalFacturaChofer!:number;
   totalFacturaProveedor!:number;
+  $tarifasClientes!: TarifaCliente[];
+  $tarifasChoferes!: TarifaChofer[];
+  $tarifasProveedores!: TarifaProveedor[];
 
-  constructor(public activeModal: NgbActiveModal, private buscarTarifaServ: BuscarTarifaService){}
+  constructor(public activeModal: NgbActiveModal, private buscarTarifaServ: BuscarTarifaService, private storageService:StorageService){}
 
   ngOnInit(): void {
     this.data = this.fromParent.item;
-    //console.log()(this.data);
+    //console.log(this.data);
+    //console.log("modo: ", this.fromParent.modo)
     switch(this.fromParent.modo){
       case "clientes":
+        this.storageService.getByFieldValue("tarifasCliente","idTarifaCliente",this.data.idTarifa);
+        this.storageService.getByFieldValue("facOpLiqChofer","operacion.idOperacion",this.data.operacion.idOperacion);    
         this.buscarTarifaClienteId();
         this.buscarTarifaChoferOp(); 
         break
       case "choferes":
+        this.storageService.getByFieldValue("tarifasChofer","idTarifa",this.data.idTarifa);
+        this.storageService.getByFieldValue("facOpLiqCliente","operacion.idOperacion",this.data.operacion.idOperacion);    
         this.buscarTarifaChoferId();
         this.buscarTarifaClienteOp(); 
         break
       case "proveedores":
+        this.storageService.getByFieldValue("tarifasProveedor","idTarifaProveedor",this.data.idTarifa);
+        this.storageService.getByFieldValue("facOpLiqCliente","operacion.idOperacion",this.data.operacion.idOperacion);    
         this.buscarTarifaProveedorId();
         this.buscarTarifaClienteOp();
         /* this.buscarTarifaChoferId()
@@ -49,6 +60,12 @@ export class ModalDetalleComponent implements OnInit {
         alert("error")
         break
     }
+
+    this.storageService.historialTarifasClientes$.subscribe(data => {
+      this.$tarifasClientes = data;
+      ////console.log("modal tarifa: ", this.$tarifasClientes);
+      
+    })
    /*  if(this.fromParent.modo === "clientes"){
       this.buscarTarifaClienteId()
       this.buscarTarifaChoferOp() 
@@ -74,22 +91,51 @@ export class ModalDetalleComponent implements OnInit {
 
   ////METODOS CUANDO SE LLAMA DE HiSTORIAL CLIENTES
   buscarTarifaClienteId(){
-    this.tarifaClienteAplicada = this.buscarTarifaServ.buscarTarifaClienteId(this.data.idTarifa)
-   // //console.log()("historial clientes/modal/tarifa CLIENTE aplicada: ", this.tarifaClienteAplicada);
-    this.montoCategoriaCliente = this.data.valorJornada
-    this.totalFacturaCliente = this.data.total
+    ////console.log("modal tarifa: ", this.$tarifasClientes);
+    
+    this.montoCategoriaCliente = this.data.valorJornada;
+    //console.log("1) historial clientes/modal/monto categoria cliente: ", this.montoCategoriaCliente);
+    this.totalFacturaCliente = this.data.total;
+    //console.log("2) historial clientes/modal/total factura cliente: ", this.totalFacturaCliente);
+    
+    this.storageService.historialTarifasClientes$.subscribe(data => {
+      this.$tarifasClientes = data;
+      this.tarifaClienteAplicada = this.$tarifasClientes[0];  
+    }); 
+    //this.tarifaClienteAplicada = this.buscarTarifaServ.buscarTarifaClienteId();    
+    //console.log("3) historial clientes/modal/tarifa CLIENTE aplicada: ", this.tarifaClienteAplicada);
   }
 
   buscarTarifaChoferOp(){
     if(this.data.operacion.chofer.proveedor === "monotributista"){
-      this.tarifaChoferAplicada = this.buscarTarifaServ.buscarTarifaChofer(this.data.operacion)
-      ////console.log()("historial clientes/modal/tarifa CHOFER aplicada: ", this.tarifaChoferAplicada);
-      this.totalFacturaChofer = this.data.montoFacturaChofer
+      //this.storageService.getByFieldValue("facOpLiqChofer","operacion.idOperacion",this.data.operacion.idOperacion)
+      this.storageService.facOpLiqChofer$.subscribe(data => {
+          let facOpLiqChofer = data;
+          let idTarifa = facOpLiqChofer[0].idTarifa
+          console.log("4)", idTarifa);
+          this.storageService.getByFieldValue("tarifasChofer", "idTarifa", idTarifa);
+          this.storageService.historialTarifas$.subscribe(data => {
+            this.$tarifasChoferes = data;
+            this.tarifaChoferAplicada = this.$tarifasChoferes[0];  
+            console.log("5) historial clientes/modal/tarifa CHOFER aplicada: ", this.tarifaChoferAplicada);
+            this.totalFacturaChofer = this.data.montoFacturaChofer;
+            console.log("6) historial clientes/modal/total factura chofer: ", this.totalFacturaChofer);
+          }); 
+      })
+      /* let idTarifa = this.buscarTarifaServ.buscarIdTarifa();
+      console.log("1)", idTarifa); */
+      
+      //this.storageService.getByFieldValue("tarifasChofer", "idTarifa", idTarifa)
+      //this.tarifaChoferAplicada = this.buscarTarifaServ.buscarTarifaChofer();
+      //this.storageService.clearInfo("tarifasChofer")
+      //console.log("historial clientes/modal/tarifa CHOFER aplicada: ", this.tarifaChoferAplicada);
+      //this.totalFacturaChofer = this.data.montoFacturaChofer
+      //console.log("historial clientes/modal/total factura chofer: ", this.totalFacturaChofer);
     } else if (this.data.operacion.chofer.proveedor !== "monotributista"){
       this.tarifaProveedorAplicada = this.buscarTarifaServ.buscarTarifaProveedor(this.data.operacion)
-     // //console.log()("historial clientes/modal/tarifa PROVEEDOR aplicada: ", this.tarifaProveedorAplicada);
+     // ////console.log()("historial clientes/modal/tarifa PROVEEDOR aplicada: ", this.tarifaProveedorAplicada);
       this.montoCategoriaProveedor = this.buscarTarifaServ.buscarCategoriaProveedor(this.tarifaProveedorAplicada, this.data.operacion.chofer.vehiculo.categoria);
-      ////console.log()(this.montoCategoriaProveedor);
+      //////console.log()(this.montoCategoriaProveedor);
       
     }    
   }
@@ -97,29 +143,72 @@ export class ModalDetalleComponent implements OnInit {
 
   ////METODOS CUANDO SE LLAMA DE HiSTORIAL CHOFERES
   buscarTarifaChoferId(){
-    this.tarifaChoferAplicada = this.buscarTarifaServ.buscarTarifaChoferId(this.data.idTarifa)
-   // //console.log()("historial choferes/modal/tarifa chofer: ", this.tarifaChoferAplicada);
-    this.totalFacturaChofer = this.data.total
-     
-     ////console.log()("categoria cliente: ", this.montoCategoriaCliente);
+    //this.tarifaChoferAplicada = this.buscarTarifaServ.buscarTarifaChoferId(this.data.idTarifa)
+    
+    this.totalFacturaChofer = this.data.total;     
+    console.log("1) categoria cliente: ", this.montoCategoriaCliente);
     this.totalFacturaCliente = this.data.montoFacturaCliente;
-    ////console.log()("total: ", this.totalFacturaCliente);
+    console.log("2) total: ", this.totalFacturaCliente);
+    this.storageService.historialTarifas$.subscribe(data => {
+      this.$tarifasChoferes = data;
+      this.tarifaChoferAplicada = this.$tarifasChoferes[0];  
+      console.log("3) historial choferes/modal/tarifa chofer: ", this.tarifaChoferAplicada);
+    }); 
   }
 
   buscarTarifaClienteOp(){
-    this.tarifaClienteAplicada = this.buscarTarifaServ.buscarTarifaCliente(this.data.operacion)
-    //console.log()("historial choferes/modal/tarifa cliente: ", this.tarifaClienteAplicada);
-    this.montoCategoriaCliente = this.buscarTarifaServ.buscarCategoriaCliente(this.tarifaClienteAplicada, this.data.operacion.chofer.vehiculo.categoria);;
-    //console.log()("monto categoria cliente: ", this.montoCategoriaCliente);
-    this.totalFacturaCliente = this.data.montoFacturaCliente;
+    this.storageService.facOpLiqCliente$.subscribe(data => {
+      let facOpLiqCliente = data;
+      let idTarifa = facOpLiqCliente[0].idTarifa
+      console.log("4)", idTarifa);
+      this.storageService.getByFieldValue("tarifasCliente", "idTarifaCliente", idTarifa);
+          this.storageService.historialTarifasClientes$.subscribe(data => {
+            this.$tarifasClientes = data;
+            this.tarifaClienteAplicada = this.$tarifasClientes[0];  
+            console.log("5) historial clientes/modal/tarifa CLIENTE aplicada: ", this.tarifaClienteAplicada);
+            this.totalFacturaCliente = this.data.montoFacturaCliente;
+            console.log("6) historial clientes/modal/total factura cliente: ", this.totalFacturaCliente);
+            this.montoCategoriaCliente = this.buscarTarifaServ.buscarCategoriaCliente(this.tarifaClienteAplicada, this.data.operacion.chofer.vehiculo.categoria);;
+            console.log("7) monto categoria cliente: ", this.montoCategoriaCliente);
+          }); 
+    })
+    //this.tarifaClienteAplicada = this.buscarTarifaServ.buscarTarifaCliente(this.data.operacion)
+    ////console.log()("historial choferes/modal/tarifa cliente: ", this.tarifaClienteAplicada);
+    //this.montoCategoriaCliente = this.buscarTarifaServ.buscarCategoriaCliente(this.tarifaClienteAplicada, this.data.operacion.chofer.vehiculo.categoria);;
+    ////console.log()("monto categoria cliente: ", this.montoCategoriaCliente);
+    //this.totalFacturaCliente = this.data.montoFacturaCliente;
   }
 
     ////METODOS CUANDO SE LLAMA DE HiSTORIAL PROVEEDORES
     buscarTarifaProveedorId(){
-      this.tarifaProveedorAplicada = this.buscarTarifaServ.buscarTarifaProveedorId(this.data.idTarifa)
-      //console.log()("historial proveedores/modal/tarifa PROVEEDOR aplicada: ", this.tarifaProveedorAplicada);
-      this.montoCategoriaProveedor = this.data.valorJornada
-      this.totalFacturaProveedor = this.data.total
+      //this.tarifaProveedorAplicada = this.buscarTarifaServ.buscarTarifaProveedorId(this.data.idTarifa)
+      ////console.log()("historial proveedores/modal/tarifa PROVEEDOR aplicada: ", this.tarifaProveedorAplicada);
+      this.montoCategoriaProveedor = this.data.valorJornada;
+      console.log("1) historial clientes/modal/monto categoria proveedor: ", this.montoCategoriaProveedor);
+      this.totalFacturaProveedor = this.data.total;
+      console.log("2) historial clientes/modal/total factura proveedor: ", this.totalFacturaProveedor);
+      this.storageService.historialTarifasProveedores$.subscribe(data => {
+        console.log("2.5",data);        
+        this.$tarifasProveedores = data;
+        console.log("2.75",this.$tarifasProveedores); 
+        this.tarifaProveedorAplicada = this.$tarifasProveedores[0];  
+        console.log("3) historial clientes/modal/tarifa PROVEEDOR aplicada: ", this.tarifaProveedorAplicada);        
+      }); 
+      //this.tarifaClienteAplicada = this.buscarTarifaServ.buscarTarifaClienteId();    
+      //console.log("3) historial clientes/modal/tarifa CLIENTE aplicada: ", this.tarifaClienteAplicada);
+      //////////////////////////////////////////////////////////
+      
+    /* this.montoCategoriaCliente = this.data.valorJornada;
+    console.log("1) historial clientes/modal/monto categoria cliente: ", this.montoCategoriaCliente);
+    this.totalFacturaCliente = this.data.total;
+    console.log("2) historial clientes/modal/total factura cliente: ", this.totalFacturaCliente);
+    
+    this.storageService.historialTarifasClientes$.subscribe(data => {
+      this.$tarifasClientes = data;
+      this.tarifaClienteAplicada = this.$tarifasClientes[0];  
+    }); 
+    //this.tarifaClienteAplicada = this.buscarTarifaServ.buscarTarifaClienteId();    
+    console.log("3) historial clientes/modal/tarifa CLIENTE aplicada: ", this.tarifaClienteAplicada); */
     }
 
     
