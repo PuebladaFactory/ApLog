8d52726b9804d4ee9a832d2c90e6fbd182888396
Nicolas Gamarra <nicogamarra2010@gmail.com>
2025-02-01 15:41:37 -0300

    liq-proveedor: baja de op: elimina op, facOpProveedor y facOpCliente

--- src/app/admin/liquidacion/liq-proveedor/liq-proveedor.component.ts
+++ src/app/admin/liquidacion/liq-proveedor/liq-proveedor.component.ts
@@ -16,6 +16,7 @@ import { Operacion } from 'src/app/interfaces/operacion';
 import Swal from 'sweetalert2';
 import { FacturarOpComponent } from '../modales/facturar-op/facturar-op.component';
 import { EditarTarifaOpComponent } from '../modales/editar-tarifa-op/editar-tarifa-op.component';
+import { ModalBajaComponent } from 'src/app/shared/modal-baja/modal-baja.component';
 
 @Component({
   selector: 'app-liq-proveedor',
@@ -387,11 +388,6 @@ export class LiqProveedorComponent implements OnInit {
     this.buscarTarifa(i);    
   }
 
-  eliminarFacturaOpCliente(factura:FacturaOp, indice:number){
-    this.removeItem(factura);
-    this.cerrarTabla(indice)
-    this.ngOnInit(); 
-  }
 
   openModalLiquidacion(): void {   
     //this.facturasLiquidadasCliente
@@ -569,5 +565,88 @@ export class LiqProveedorComponent implements OnInit {
         }
       });
     }
+
+    bajaOp(factura:FacturaOp, indice:number){
+            Swal.fire({
+                  title: "¿Desea anular la operación?",
+                  //text: "No se podrá revertir esta acción",
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonColor: "#3085d6",
+                  cancelButtonColor: "#d33",
+                  confirmButtonText: "Confirmar",
+                  cancelButtonText: "Cancelar"
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                    this.dbFirebase
+                      .obtenerTarifaIdTarifa("operaciones", factura.idOperacion, "idOperacion")
+                      .pipe(take(1)) // Asegúrate de que la suscripción se complete después de la primera emisión
+                      .subscribe(data => {      
+                          this.operacion = data;
+                          //console.log("OPERACION: ", this.operacion);
+                          this.openModalBaja(factura, indice)
+                      });               
+                  }
+                });
+            
+          }
+        
+          openModalBaja(factura:FacturaOp, indice:number){
+            {
+              const modalRef = this.modalService.open(ModalBajaComponent, {
+                windowClass: 'myCustomModalClass',
+                centered: true,
+                scrollable: true, 
+                size: 'sm',     
+              });       
+              console.log("factura",factura);
+              let info = {
+                modo: "liquidaciones",
+                item: this.operacion,
+              }  
+              
+              
+              modalRef.componentInstance.fromParent = info;
+            
+              modalRef.result.then(
+                (result) => {
+                  console.log("result", result);
+                  if(result !== undefined){   
+                      //BAJA DE FACTURA OP CHOFER
+                      this.removeItem(factura);              
+                     
+                      //BAJA DE FACTURA OP CLIENTE
+                      this.dbFirebase.obtenerTarifaMasReciente("facturaOpCliente", factura.contraParteId, "idFacturaOp", "idFacturaOp").subscribe(data => {
+                        if(data){
+                          let facturaContraParte = data
+                          console.log("facturaContraParte: ", factura);
+                          this.storageService.deleteItem("facturaOpCliente", facturaContraParte, factura.contraParteId, "INTERNA", "");        
+                        }
+                        
+                      })
+                      
+                      //BAJA DE OP
+                      this.storageService.deleteItemPapelera("operaciones", this.operacion, this.operacion.idOperacion, "Baja", `Baja de operacion ${this.operacion.idOperacion} desde Liquidaciones`, result);    
+                      
+                      this.cerrarTabla(indice)
+                      this.ngOnInit(); 
+                      //////////////////
+        
+        
+                      ////////console.log("llamada al storage desde op-abiertas, deleteItem");
+        
+                      ////////console.log("consultas Op: " , this.$consultasOp);
+                      Swal.fire({
+                      title: "Confirmado",
+                      text: "La operación ha sido dada de baja",
+                      icon: "success"
+                      });
+                  }
+                  
+                },
+                (reason) => {}
+              );
+            }
+          }
 
 }
